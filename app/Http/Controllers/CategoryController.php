2add22aa1fc75cc6522e0e84db44fbfd1a854944
ahmadelhajj1996 <ahmad.1996.elhaj@gmail.com>
2025-08-27 22:44:58 +0300

    Initial Laravel 10 project commit 1

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 use App\Models\Category;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use App\Traits\ApiResponseTrait;
-
 use Illuminate\Http\Request;
+
 
 class CategoryController extends Controller
 {
 
     use ApiResponseTrait;
+
+    private function validateRequest(Request $request, array $rules)
+    {
+        try {
+            return $request->validate($rules);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            throw new HttpResponseException(
+                $this->errorResponse(__('messages.validation_failed'), 'messages', 422, [
+                    'errors' => $e->errors(),
+                ])
+            );
+        }
+    }
 
     public function index(Request $request)
     {
@@ -17,5 +31,42 @@ class CategoryController extends Controller
         $categories = Category::get();
         return $this->successResponse($categories, 'messages', 'categoriesـretrievedـsuccessfully');
     }
-    
+
+    public function store(Request $request)
+    {
+        $data = $this->validateRequest($request, [
+            'title' => 'required|string|max:255',
+        ]);
+
+
+        $category = Category::create($data);
+
+        return $this->successResponse($category, 'messages', 'created');
+    }
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+        if (!$category) {
+            return $this->errorResponse(__('not_found'), 'messages', 404);
+        }
+
+        $data = $this->validateRequest($request, [
+            'title' => 'sometimes|string',
+
+        ]);
+
+        $category->update($data);
+
+        return $this->successResponse($category, 'messages', 'updated');
+    }
+     public function destroy($id)
+    {
+        $category = Category::find($id);
+        if (!$category) {
+            return $this->errorResponse(__('not_found'), 'messages', 404);
+        }
+        $category->delete();
+
+        return $this->successResponse([], 'messages', 'deleted');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,8 @@ https://phplaravel-1483035-5810347.cloudwaysapps.com/
 
 
 Route::get('categories', [CategoryController::class, 'index']);
+Route::post('categories', [CategoryController::class, 'store']);
+Route::put('categories/{category}', [CategoryController::class, 'update']);
+Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
 
 
